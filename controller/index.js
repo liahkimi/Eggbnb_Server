3a7563971bcc578cc87c2effd50117b3d604eee1
maruanna <maruanna1994@gmail.com>
2024-07-12 13:29:58 +0900

    원래대로 돌려놓기

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,7 @@
 
 const index = async (req, res) => {
     res.set({"Content-Type": "text/html; charset=utf-8"});
-    res.end("<h1>hi!!</h1>");
+    res.end("<h1>hi</h1>");
 };
 
 export { index };
